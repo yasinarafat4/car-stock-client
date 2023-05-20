94793e0cff92f101919bff707407acd63d11e8a5
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-20 22:28:02 +0600

    Implementing search system on the All Toys page has done

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -4,6 +4,7 @@ import { FiSearch } from "react-icons/fi";
 
 const AllToys = () => {
   const [allToys, setAllToys] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/addToys")
@@ -12,6 +13,16 @@ const AllToys = () => {
         setAllToys(data);
       });
   }, []);
+
+  // For Search
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/searchToys/${searchText}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllToys(data);
+      });
+  };
+
   return (
     <div>
       <div className="md:flex justify-between">
@@ -20,11 +31,15 @@ const AllToys = () => {
         </h2>
         <div className="flex justify-center items-center mb-5 md:0">
           <input
+            onChange={(e) => setSearchText(e.target.value)}
             type="text"
             placeholder="Search..."
             className="h-10 py-2 px-4 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           />
-          <button className="bg-blue-500 hover:bg-blue-600 text-white h-10 px-4 rounded-r">
+          <button
+            onClick={handleSearch}
+            className="bg-blue-500 hover:bg-blue-600 text-white h-10 px-4 rounded-r"
+          >
             <FiSearch className="h-5 w-5" />
           </button>
         </div>
