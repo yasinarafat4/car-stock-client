08944409e2ed16aeb4bda98300ec305f17bd574b
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-20 17:14:23 +0600

    Showing toys on the My Toys page that only one user has added

--- src/pages/TabularToy/TabularToy.jsx
+++ src/pages/TabularToy/TabularToy.jsx
@@ -2,7 +2,7 @@ const TabularToy = ({ toy }) => {
   console.log(toy);
   const { name, sellerName } = toy;
   return (
-    <div className="my-4 bg-red-400">
+    <div className="my-4 bg-red-400 px-4 py-6 rounded-lg">
       <p>Name:{name}</p>
       <p>Seller Name:{sellerName}</p>
     </div>
